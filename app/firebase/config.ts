e15563710cd01a560ecc73064e7d589be04451af
Victor Ezekiel <pianistpro8@gmail.com>
2024-08-16 08:16:51 +0100

    new deployment

--- app/firebase/config.ts
+++ app/firebase/config.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDvqiHsOGYeUSu46Et_XhXDdLB-IE9PyWU",
-    authDomain: "vcode-01.firebaseapp.com",
-    projectId: "vcode-01",
-    storageBucket: "vcode-01.appspot.com",
-    messagingSenderId: "1055866201293",
-    appId: "1:1055866201293:web:49f8a5e3f38740cd9f7584"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_API_ID
 };
 
 const app = initializeApp(firebaseConfig);
