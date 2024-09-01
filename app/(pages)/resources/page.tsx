dab8467c8a1e2398720f0d89c4c91ff87d6f8754
Victor Ezekiel <pianistpro8@gmail.com>
2024-09-01 22:23:11 +0100

    trial commit

--- app/(pages)/resources/page.tsx
+++ app/(pages)/resources/page.tsx
@@ -2,28 +2,21 @@ import { ref, listAll } from "firebase/storage";
 import { storage } from "@/app/firebase/config"
 import Card from "./card";
 
-
 const Resources = async () => {
     const listRef = ref(storage, 'resources');
-
-    const getResources = async () => {
-        const data: string[] = []
-        const res = await listAll(listRef)
-        res.items.forEach(item => {
-            data.push(item.name)
-        })
-        return data
-    }
-
-    const listOfResources = await getResources()
+    const data: string[] = []
+    const res = await listAll(listRef)
+    res.items.forEach(item => {
+        data.push(item.name)
+    })
 
     return (
         <section>
             <header className="p-3 text-center">
-                Download useful materials to enhance your growth, ALL FOR FREE
+                Download useful materials to enhance your growth, <strong>ALL FOR FREE</strong>
             </header>
             <fieldset className="flex flex-wrap gap-3 md:gap-5 justify-center max-w-[90dvw] md:max-w-[70%] mx-auto mt-10 px-10">
-                {listOfResources?.map((item, itemIdx) => (
+                {data?.map((item, itemIdx) => (
                     <Card key={itemIdx} item={item} />
                 ))}
             </fieldset>
